48792f7ebfbb198cf62b0781c2d65074a9d16b59
Drivello <Nico_dd@outlook.com.ar>
2025-07-06 19:33:44 -0300

    user repo config

--- services/user/config/config.go
+++ services/user/config/config.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"log"
+	"github.com/spf13/viper"
+)
+
+// Config holds all configuration for the UserService.
+type Config struct {
+	PostgresDSN  string
+	RedisAddr    string
+	KafkaBrokers []string
+	ServicePort  string
+}
+
+func LoadConfig() *Config {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("No .env file found, relying on environment variables")
+	}
+
+	cfg := &Config{
+		PostgresDSN:  viper.GetString("POSTGRES_DSN"),
+		RedisAddr:    viper.GetString("REDIS_ADDR"),
+		KafkaBrokers: viper.GetStringSlice("KAFKA_BROKERS"),
+		ServicePort:  viper.GetString("USER_SERVICE_PORT"),
+	}
+
+	if cfg.ServicePort == "" {
+		cfg.ServicePort = "8082"
+	}
+	return cfg
+}
