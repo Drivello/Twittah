9ab835ba46c924eb81099d50d027317c26ac1f0a
Drivello <Nico_dd@outlook.com.ar>
2025-07-07 23:46:09 -0300

    usecases

--- services/user/internal/usecase/follow_user.go
+++ services/user/internal/usecase/follow_user.go
@@ -8,16 +8,26 @@ import (
 )
 
 type FollowUserUseCase struct {
-	Repo ports.UserRepository
+	Repo   ports.UserRepository
+	Events ports.UserEventPublisher
 }
 
-func NewFollowUserUseCase(repo ports.UserRepository) *FollowUserUseCase {
-	return &FollowUserUseCase{Repo: repo}
+func NewFollowUserUseCase(repo ports.UserRepository, events ports.UserEventPublisher) *FollowUserUseCase {
+	return &FollowUserUseCase{Repo: repo, Events: events}
 }
 
 func (uc *FollowUserUseCase) Execute(ctx context.Context, followerID, followeeID string) error {
 	if followerID == followeeID {
 		return errors.New("cannot follow yourself")
 	}
-	return uc.Repo.FollowUser(ctx, followerID, followeeID)
+	err := uc.Repo.FollowUser(ctx, followerID, followeeID)
+	if err != nil {
+		return err
+	}
+	// Publish event (fire and forget, but log error)
+	if pubErr := uc.Events.PublishFollow(followerID, followeeID); pubErr != nil {
+		// TODO: Connect with timeline
+		// Log error if needed, or ignore for now
+	}
+	return nil
 }
--- services/user/internal/usecase/unfollow_user.go
+++ services/user/internal/usecase/unfollow_user.go
@@ -8,16 +8,26 @@ import (
 )
 
 type UnfollowUserUseCase struct {
-	Repo ports.UserRepository
+	Repo   ports.UserRepository
+	Events ports.UserEventPublisher
 }
 
-func NewUnfollowUserUseCase(repo ports.UserRepository) *UnfollowUserUseCase {
-	return &UnfollowUserUseCase{Repo: repo}
+func NewUnfollowUserUseCase(repo ports.UserRepository, events ports.UserEventPublisher) *UnfollowUserUseCase {
+	return &UnfollowUserUseCase{Repo: repo, Events: events}
 }
 
 func (uc *UnfollowUserUseCase) Execute(ctx context.Context, followerID, followeeID string) error {
 	if followerID == followeeID {
 		return errors.New("cannot unfollow yourself")
 	}
-	return uc.Repo.UnfollowUser(ctx, followerID, followeeID)
+	err := uc.Repo.UnfollowUser(ctx, followerID, followeeID)
+	if err != nil {
+		return err
+	}
+	// Publish event (fire and forget, but log error)
+	if pubErr := uc.Events.PublishUnfollow(followerID, followeeID); pubErr != nil {
+		// TODO: Connect with timeline
+		// Log error if needed, or ignore for now
+	}
+	return nil
 }
