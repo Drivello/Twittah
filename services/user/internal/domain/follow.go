06844a05590f2aa996b2402fab843dee44ebb39e
Drivello <Nico_dd@outlook.com.ar>
2025-07-07 23:45:49 -0300

    deletions

--- services/user/internal/domain/follow.go
+++
@@ -1,8 +0,0 @@
-// Follow domain entity for Twittah
-package domain
-
-// Follow represents a follow relationship between users.
-type Follow struct {
-	FollowerID string // The user who follows
-	FolloweeID string // The user being followed
-}
