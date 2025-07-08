ee7e953f57e9b785b06426f1c76de8efe760aa9b
Drivello <Nico_dd@outlook.com.ar>
2025-07-07 23:47:18 -0300

    domain

--- services/user/internal/domain/user.go
+++ services/user/internal/domain/user.go
@@ -3,7 +3,6 @@ package domain
 
 // User represents a user in the Twittah platform.
 type User struct {
-	ID       string // Unique user identifier
-	Username string // Username
-	// Add other user fields as needed
+	ID       string
+	Username string
 }
