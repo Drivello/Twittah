39a4702724764e02634845db68707f7e9f36cf18
Drivello <Nico_dd@outlook.com.ar>
2025-07-07 22:23:34 -0300

    user config

--- services/user/config/config.go
+++ services/user/config/config.go
@@ -2,6 +2,11 @@ package config
 
 import (
 	"log"
+	"context"
+	"strings"
+
+	"go.uber.org/zap"
+	"github.com/Drivello/Twittah/services/user/ent"
 	"github.com/spf13/viper"
 )
 
@@ -11,6 +16,31 @@ type Config struct {
 	RedisAddr    string
 	KafkaBrokers []string
 	ServicePort  string
+}
+
+func InitLogger() *zap.Logger {
+	logger, _ := zap.NewProduction()
+	zap.ReplaceGlobals(logger)
+	return logger
+}
+
+func InitEntClient(postgresDSN string) (*ent.Client, error) {
+	client, err := ent.Open("postgres", postgresDSN)
+	if err != nil {
+		return nil, err
+	}
+	if err := client.Schema.Create(context.Background()); err != nil {
+		client.Close()
+		return nil, err
+	}
+	return client, nil
+}
+
+func ParseKafkaBrokers(brokers []string) []string {
+	if len(brokers) == 1 && strings.Contains(brokers[0], ",") {
+		return strings.Split(brokers[0], ",")
+	}
+	return brokers
 }
 
 func LoadConfig() *Config {
