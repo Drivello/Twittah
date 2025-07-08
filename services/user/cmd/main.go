79d9f9c9de3dcfc9d259bc57f3bb9c68a6dd65c4
Drivello <Nico_dd@outlook.com.ar>
2025-07-07 23:49:49 -0300

    user main

--- services/user/cmd/main.go
+++ services/user/cmd/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"context"
-
 	"github.com/Drivello/Twittah/services/user/config"
 	"github.com/Drivello/Twittah/services/user/internal/adapters/http"
 	"github.com/Drivello/Twittah/services/user/internal/adapters/kafka"
 	"github.com/Drivello/Twittah/services/user/internal/adapters/postgres"
 	"github.com/Drivello/Twittah/services/user/internal/usecase"
 
-	_ "github.com/lib/pq"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -31,16 +29,11 @@ func main() {
 	followUC := usecase.NewFollowUserUseCase(repo, nil)
 	unfollowUC := usecase.NewUnfollowUserUseCase(repo, nil)
 
-	consumer, err := kafka.NewUserEventConsumer(brokers, "user-service-group", "follows", followUC, unfollowUC)
+	producer, err := kafka.StartAllKafkaConsumers(repo, brokers, logger)
 	if err != nil {
-		logger.Sugar().Fatalw("failed to create kafka consumer", "error", err)
+		logger.Sugar().Fatalw("failed to start Kafka consumers", "error", err)
 	}
-	ctx := context.Background()
-	go func() {
-		if err := consumer.Start(ctx); err != nil {
-			logger.Sugar().Fatalw("kafka consumer stopped", "error", err)
-		}
-	}()
+	defer producer.Close()
 
 	handler := http.NewUserHandler(followUC, unfollowUC)
 	r := gin.Default()
@@ -54,4 +47,3 @@ func main() {
 		logger.Sugar().Fatalw("failed to run http server", "error", err)
 	}
 }
-
